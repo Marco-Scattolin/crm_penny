91039ad7e48b7b0e850dfd79b181f1bba9663f2a
Marco-Scattolin <scattolinmarco20@gmail.com>
2024-09-26 22:29:05 +0200

    add acount

--- crm_project/account/models.py
+++ crm_project/account/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('webdev', 'Web Developer'),
+        ('designer', 'UI/UX Designer'),
+        ('copywriter', 'Copywriter'),
+        ('social_media', 'Social Media Manager'),
+        ('strategy', 'Strategia Digitale e Analisi Dati'),
+        ('admin', 'Amministratore'),
+        ('dev', 'Sviluppo'),
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='webdev')
---
+++ crm_project/account/settings.py
@@ -0,0 +1,4 @@
+INSTALLED_APPS = [
+    # Altre app di Django
+    'accounts',
+]
